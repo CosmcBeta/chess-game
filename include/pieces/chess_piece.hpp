d2e43598274199b8dd63b4f94e528976f471a6e3
Riley <100080465+CosmcBeta@users.noreply.github.com>
2025-11-11 14:54:48 -0600

    refactored the chess piece base class

---
+++ include/pieces/chess_piece.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "piece_info.hpp"
+
+#include <vector>
+
+class Piece;
+using Board = std::array<std::array<Piece*, 8>, 8>;
+// const int SQUARE_SIZE = 80;
+// static constexpr float SCALE = 0.24024024f;
+
+class Piece
+{
+public:
+	Piece(Team team, PieceType type, Position position);
+
+	virtual void calculateMoves(Board board, Move previousMove) = 0;
+
+	Team getTeam() const;
+	PieceType getPieceType() const;
+	Position getPosition() const;
+	std::vector<Move> getMoves() const;
+	bool getFirstMove() const;
+	bool getEnPassant() const;
+
+	void setPosition(Position position);
+	void setFirstMove(bool firstMove);
+	void setEnPassant(bool enPassant);
+
+protected:
+	Position position_; // Position on an 8x8 scale
+	Team team_;
+	PieceType pieceType_;
+	std::vector<Move> possibleMoves_;
+	bool firstMove_;
+	bool canEnPassant_;
+};
---
+++ src/pieces/chess_piece.cpp
@@ -0,0 +1,52 @@
+#include "pieces/chess_piece.hpp"
+#include "pieces/piece_info.hpp"
+
+Piece::Piece(Team team, PieceType type, Position position)
+	:team_(team), pieceType_(type), firstMove_(true),
+	canEnPassant_(false), position_(position)
+{}
+
+Team Piece::getTeam() const
+{
+    return team_;
+}
+
+PieceType Piece::getPieceType() const
+{
+    return pieceType_;
+}
+
+Position Piece::getPosition() const
+{
+    return position_;
+}
+
+std::vector<Move> Piece::getMoves() const
+{
+    return possibleMoves_;
+}
+
+void Piece::setFirstMove(bool firstMove)
+{
+    firstMove_ = firstMove;
+}
+
+bool Piece::getFirstMove() const
+{
+    return firstMove_;
+}
+
+void Piece::setEnPassant(bool enPassant)
+{
+    canEnPassant_ = enPassant;
+}
+
+bool Piece::getEnPassant() const
+{
+    return canEnPassant_;
+}
+
+void Piece::setPosition(Position position)
+{
+	position_ = position;
+}
